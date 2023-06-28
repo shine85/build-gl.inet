797f84da5d398e7fe3fc4f335e1250fdbd7409eb
papagaye744 <132003569+papagaye744@users.noreply.github.com>
2023-06-29 00:19:32 +0800

    add datconf source

--- build.sh
+++ build.sh
@@ -75,6 +75,8 @@ function build_firmware(){
     # add fullcorenat patch
     # mkdir package/network/config/firewall/patches
     # cp $CRTDIR/fullconenat.patch package/network/config/firewall/patches/fullconenat.patch
+    # add luci-app-mtk deps
+    cp -r $CRTDIR/openwrt/dl/* dl/
     #install feed 
     ./scripts/feeds update -a && ./scripts/feeds install -a && make defconfig
     #build 
